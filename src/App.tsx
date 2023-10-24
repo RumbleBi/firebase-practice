86000d11a292a437a5a6bb166b06f771c28219a3
RumbleBi <olzlel2000@gmail.com>
2023-10-24 21:50:35 +0900

    routes & globalStyle setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import { GlobalStyles } from "./GlobalStyles";
+import { router } from "./components/routes/config";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/routes/config.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout";
+import Home from "./home";
+import Profile from "./profile";
+import Login from "./login";
+import Signup from "./signup";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
